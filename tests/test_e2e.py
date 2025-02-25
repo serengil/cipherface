d0e21f954802c892f76f879d6f7cb7f8eaa594a5
Sefik Ilkin Serengil <serengil@gmail.com>
2025-02-25 18:50:09 +0000

    return type test securely embed

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -16,7 +16,6 @@ public_key_path = os.path.join(temp_dir, "public.txt")
 
 
 def test_e2e():
-
     model_names = ["VGG-Face", "Facenet", "Facenet512"]
     distance_metrics = ["euclidean", "cosine"]
 
@@ -30,7 +29,10 @@ def test_e2e():
             database = {}
             for img_path in img_paths:
                 embeddings = onprem.securely_embed(img_path=img_path)
+                # return type should be list of str for single input
+                assert isinstance(embeddings, list)
                 for embedding in embeddings:
+                    assert isinstance(embedding, str)
                     database[img_path] = embedding
                     break
 
@@ -75,3 +77,18 @@ def test_e2e():
                 ), f"{img_path} is misclassified. Expected {expected_classifications[idx]}, but got {is_verified}"
 
             logger.info(f"✅ e2e euclidean test done for {model_name} - {distance_metric}")
+
+
+def test_many_inputs_to_securely_embed():
+    img_paths = ["dataset/img1.jpg", "dataset/img2.jpg", "dataset/img3.jpg"]
+
+    onprem = CipherFace(model_name="Facenet", distance_metric="euclidean")
+    embeddings = onprem.securely_embed(img_path=img_paths)
+    # return type should be list of list of str for multiple inputs
+    assert isinstance(embeddings, list)
+    for embedding in embeddings:
+        assert isinstance(embedding, list)
+        for emb in embedding:
+            assert isinstance(emb, str)
+
+    logger.info("✅ securely embed multiple inputs test done")
